bb01d7cb2b90db8a853b52ec4f1146d7bbee7363
jerrypena1306-eng <jerrypena1306@gmail.com>
2026-04-26 15:35:33 -0600

    Update index.js

--- index.js
+++ index.js
@@ -3,16 +3,13 @@ const app = express();
 
 app.use(express.json());
 
-app.post("/chat", (req, res) => {
-    const mensaje = req.body.message;
-
-    const respuesta = "NPC 😎 dice: " + mensaje;
-
-    res.json({ reply: respuesta });
+app.get("/", (req, res) => {
+    res.send("Servidor activo ");
 });
 
-app.get("/", (req, res) => {
-    res.send("Servidor activo");
+app.post("/chat", (req, res) => {
+    const mensaje = req.body.message;
+    res.json({ reply: "NPC dice: " + mensaje });
 });
 
 app.listen(3000, () => {
