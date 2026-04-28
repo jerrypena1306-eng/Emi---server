523363f41852c8f9da2c3eca46d157af4121c230
jerrypena1306-eng <jerrypena1306@gmail.com>
2026-04-28 01:01:37 +0000

    index.js

--- index.js
+++ index.js
@@ -4,14 +4,25 @@ const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("Servidor activo ");
+    res.send("OK");
 });
 
 app.post("/chat", (req, res) => {
-    const mensaje = req.body.message;
-    res.json({ reply: "NPC dice: " + mensaje });
+    const msg = req.body.message || "";
+
+    let respuesta = "No entendí ";
+
+    if (msg.toLowerCase().includes("hola")) {
+        respuesta = "Hola humano ";
+    } else if (msg.toLowerCase().includes("adios")) {
+        respuesta = "Nos vemos ";
+    } else {
+        respuesta = "Dijiste: " + msg;
+    }
+
+    res.json({ reply: respuesta });
 });
 
-app.listen(3000, () => {
-    console.log("Servidor listo");
+app.listen(5000, "0.0.0.0", () => {
+    console.log("Brain listo");
 });
